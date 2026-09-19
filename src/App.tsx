1cc1a18c38aeac5e5ee14f0a7f33c1331c612f4e
Anudeep B <anudeepb110@gmail.com>
2026-09-19 13:09:18 +0530

    Working with Type Predicates & Facing TypeScript Limitations

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
     {/* <Input id="name" label="Your Name" type="text"></Input>
     <Input id="age" label="Your Age" type="number"></Input> */}
     <p>
-      <Button className="button" el="button">A Button</Button>
+      <Button>A Button</Button>
     </p>
     <p>
-      <Button className="button" el="anchor" href="https://www.google.com">A link</Button>
+      <Button href="https://www.google.com">A link</Button>
     </p>
   </main>)
 }
