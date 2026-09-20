8c525ec2485d72f6ce44bfc0ab9c83ed5195b13f
Anudeep B <anudeepb110@gmail.com>
2026-09-20 10:12:25 +0530

    Reset form from form component

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,16 +1,18 @@
-import type { ComponentPropsWithoutRef, FormEvent, SyntheticEvent } from "react";
+import { useRef, type ComponentPropsWithoutRef, type FormEvent, type SyntheticEvent } from "react";
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value:unknown) => void;
 };
 const Form = ({onSave, children, ...otherProps}:FormProps) => {
+    const form = useRef<HTMLFormElement>(null);
     const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget)
         const data = Object.fromEntries(formData);
         onSave(data)
+        form.current?.reset();
     };    
-    return (<form onSubmit={handleOnSubmit} {...otherProps}>{children}</form>)
+    return (<form onSubmit={handleOnSubmit} {...otherProps} ref={form}>{children}</form>)
 };
 
 export default Form;
